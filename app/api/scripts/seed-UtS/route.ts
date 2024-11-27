21fa3f671b21e873d682d095d89b0e144c6241d6
Antonov-S <anto1nov1@gmail.com>
2024-11-27 02:59:38 +0200

    Split seeding skills and userToSkills route in two to prevent timeout error while seeding data

--- app/api/scripts/seed-UtS/route.ts
+++ app/api/scripts/seed-UtS/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
 
-import seedSkills from "@/scripts/seed-skills";
+import seedUsersToSkills from "@/scripts/seed-users-to-skills";
 
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-  await seedSkills();
+  await seedUsersToSkills();
   return NextResponse.json({ message: "success" });
 }
